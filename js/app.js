9a3c1ea18a42f49b66ae1541baeb674371193548
Victor Bueno <victorbueno2003@hotmail.com>
2023-02-20 18:27:29 -0300

    adicionado função de notificação de cada 1hr

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
-import { Timer } from "./timer.js"
-import { Notifyer } from "./Notifyer.js"
+import { Notifyer } from './Notifyer.js';
+import { Timer } from './Timer.js';
+import { Emitter } from './Emitter.js';
+
+const notify = Notifyer.notify({
+  title: "Lista de Tarefas",
+  body: "Ainda possuem tarefas a serem feitas!",
+  icon:"img\lista-de-controle.png"
+})
+
 const App = {
-async start(){
- try{
-    const time= 25* 60;
-    Timer.init(time)
-    await Notifyer.init()
-    Notifyer.notify({
-        title:"tarefas",
-        body:"Existem tarefas para fazer"
-    })
- }catch (err){
-    console.log(err.message)
- }
+  async start() {
+   try {
+     await Notifyer.init()
+
+    Emitter.on('countdown-start', notify)
+    Emitter.on('countdown-end', Timer.init)
+    
+    Timer.init()
+   } catch (err) {
+     console.log(err.message)
+   }
+  }
 }
-}
-export{App}
+
+export { App }
